4649c28f3dd0e32ed7d8e11753ae2768b3524f4e
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-04-26 14:38:16 -0400

    Review DAO

--- Models/ReviewsModel.js
+++ Models/ReviewsModel.js
@@ -4,6 +4,6 @@ const schema = mongoose.Schema({
   review: { type: String, default: "" },
   rating: { type: Number, required: true, default: 0 },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-  product: { type: mongoose.Schema.Types.ObjectId },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Products" },
 });
 export default mongoose.model("Reviews", schema);
