0c22efc3cd5410982bc81217a69e256694a53af7
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-04-27 10:34:20 -0400

    merge Changes in UserController

---
+++ DAO/ProductsDao.js
@@ -0,0 +1,21 @@
+import ProductsModel from "../Models/ProductsModel.js";
+
+export const findAllProductPdao = () => {
+  ProductsModel.find({});
+};
+export const findOneProductPdao = (id) => {
+  ProductsModel.find({ _id: id });
+};
+export const createProductPdao = (id) => {
+  ProductsModel.create(id);
+};
+
+export const updateLikePdao = (aid, lid) =>
+  ProductsModel.updateOne({ _id: aid }, { $push: { likes: lid } });
+export const removeLikePdao = (aid, lid) =>
+  ProductsModel.updateOne({ _id: aid }, { $pull: { likes: lid } });
+
+export const updateReviewPdao = (aid, rid) =>
+  ProductsModel.updateOne({ _id: aid }, { $push: { reviews: rid } });
+export const removeReviewPdao = (aid, rid) =>
+  ProductsModel.updateOne({ _id: aid }, { $pull: { reviews: rid } });
