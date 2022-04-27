ebb21654fd26a1d4fe4398cba4a7600e2a56fb42
BooraKaushik <87960952+BooraKaushik@users.noreply.github.com>
2022-04-27 10:32:42 -0400

    Revert "Review DAO"
    This reverts commit 4649c28f3dd0e32ed7d8e11753ae2768b3524f4e.

--- DAO/reviewDao.js
+++ DAO/reviewDao.js
@@ -1,6 +1,13 @@
 import ReviewsModel from "../Models/ReviewsModel.js";
 export const findAllReviewsRdao = () => ReviewsModel.find();
-export const findOneReviewsRdao = (userId, productId) =>
-  ReviewsModel.findOne({ user: userId, product: productId });
-export const CreateReviewRdao = (review) => ReviewsModel.create(review);
-export const deleteLikeRdao = (rid) => ReviewsModel.deleteOne({ _id: aid });
+export const findOneAddressAdao = (userId, productId) =>
+  AddressModel.findOne({ user: userId, product: productId });
+export const createAddressAdao = (address) => AddressModel.create(address);
+export const deleteAddressAdao = (aid) => AddressModel.deleteOne({ _id: aid });
+export const updateAddressAdao = (aid, address) =>
+  AddressModel.updateOne({ _id: aid }, { $set: address });
+
+export const updateUserAdao = (aid, uid) =>
+  AddressModel.updateOne({ _id: aid }, { $push: { users: uid } });
+export const removeUserAdao = (aid, uid) =>
+  AddressModel.updateOne({ _id: aid }, { $pull: { users: uid } });
