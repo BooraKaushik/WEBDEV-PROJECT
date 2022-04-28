3c6078a79e3323f0e444e8a5ea4c9b56afa3a200
abhishekkumar <abhi.kr0550@gmail.com>
2022-04-28 18:05:23 -0400

    product schema

--- Models/ProductsModel.js
+++ Models/ProductsModel.js
@@ -4,6 +4,14 @@ const schema = mongoose.Schema({
   name: { type: String, required: true },
   imageUrl: { type: String, required: true },
   price: { type: String, required: true },
+  manufacturer: { type: String },
+  asin: { type: String },
+  country: { type: String},
+  originalPrice: { type: Number, min: 0 },
+  discount: { type: Number, min: 0, max: 100 },
+  discountPercentage: { type: Number, min: 0 },
+  currency: { type: String}
+
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Likes", default: [] }],
   reviews: [
     { type: mongoose.Schema.Types.ObjectId, ref: "Reviews", default: [] },
